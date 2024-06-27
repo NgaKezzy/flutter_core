43d26cf5502dda303a091cc89b539453f43f8dae
Ngakezzy <ngakezzy@gmail.com>
2024-06-27 21:58:00 +0700

    Thêm logger dio

--- lib/net_work/api_service.dart
+++ lib/net_work/api_service.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 enum DioMethod { post, get, put, delete }
 
@@ -25,6 +26,14 @@ class APIService {
           },
         ),
       );
+      dio.interceptors.add(PrettyDioLogger(
+          requestHeader: true,
+          requestBody: true,
+          responseBody: true,
+          responseHeader: false,
+          error: true,
+          compact: true,
+          maxWidth: 90));
       switch (method) {
         case DioMethod.post:
           return dio.post(
